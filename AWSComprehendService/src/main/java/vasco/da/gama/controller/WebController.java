87193ea4b204dc63f1a467e04268e9173e2059f2
Wes Hediger <wes.hediger@gmail.com>
2018-11-28 19:35:27 -0600

    add WebController

---
+++ AWSComprehendService/src/main/java/vasco/da/gama/controller/WebController.java
@@ -0,0 +1,14 @@
+package vasco.da.gama.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WebController {
+
+  @RequestMapping("/home")
+  public String home(){
+    return "home.html";
+  }
+}
